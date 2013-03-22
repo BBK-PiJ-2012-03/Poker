fe444dc8e9dfff05154a5c429dbdee9543aaffbb
Victor de los Santos <vis.bcn@gmail>
2013-03-22 14:54:46 +0000

    Fixed the Suit.java after the sync

--- Poker/poker/Suit.java
+++ Poker/poker/Suit.java
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-package poker;
-
-public enum Suit {
-	DIAMONDS("\4"), // "\4" == ♦,
-	CLUBS("\5"), // "\5" == ♣,
-	HEARTS("\3"), // "\3" == ♥,
-	SPADES("\6"); // "\6" == ♠,
-	// The string outputs the correct symbol in the command line
-	
-	private String suitCharacter;
-	
-	private Suit(String suitCharacter){
-		this.suitCharacter = suitCharacter;
-	}
-	public String getSuitCharacter(){
-		return suitCharacter;
-	}
-}
-=======
 package poker;
 
 public enum Suit {
@@ -38,4 +18,3 @@ public enum Suit {
 	}
 	
 }
->>>>>>> branch 'master' of https://github.com/BBK-PiJ-2012-03/Poker.git
