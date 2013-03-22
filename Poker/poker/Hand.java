4456cc66a9606bcd3b1654409b1c02be0a02cc9c
Victor de los Santos <vis.bcn@gmail>
2013-03-22 14:27:09 +0000

    Added comments to the interfaces Hand and Card

--- Poker/poker/Hand.java
+++ Poker/poker/Hand.java
@@ -2,7 +2,17 @@ package poker;
 
 public interface Hand {
 
+	/**
+	 * Returns the category of the hand
+	 * 
+	 * @return Category of the hand
+	 */
 	public Category handCategory();
 	
+	/**
+	 * Returns an array with the cards in the hand
+	 * 
+	 * @return Card[] array with the cards in Hand
+	 */
 	public Card[] cardsInHand();
 }
