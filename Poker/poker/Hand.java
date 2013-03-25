c70eb0b56b825fb338015b6c0f905c5745d3cbdf
Alex <a.rokotas@gmail.com>
2013-03-25 18:26:49 +0000

    Added comments at Hand. Now only Game needs to be finished.

--- Poker/poker/Hand.java
+++ Poker/poker/Hand.java
@@ -1,5 +1,12 @@
 package poker;
-
+ /**
+ * Hand is a poker hand. It consists of an array of 5 cards and a Category of the hand.
+ * Hand is evaluated and sorted at construction time (evaluateHand) is called by the constructor.
+ * It is sorted in a way that puts all the cards that make the Category of the hand at the beginning of the Card array. (for example Pairs or Three_of_a_Kind)
+ * and the rest cards are sorted from higher to lower.
+ * This way its easy for the hand to be evaluated and compared with other hands
+ * It also makes changing card easy because most of the times the cards that should be changed are at the end of the hand.
+ */
 public interface Hand {
 
 	/**
@@ -15,13 +22,6 @@ public interface Hand {
 	 * @return Card[] array with the cards in Hand
 	 */
 	public Card[] getHand();
-	
-	/*
-	 * Return an String with the hand
-	 * 
-	 * @return String with the hand
-	 */
-	public String toString();
 
 	/*
 	 * Set an undefined number of cards in the hand. It will start changing the cards from the end to the beginning 
